5b56f00b5e2148f0134d8e05b64e0b345cfbc202
mitchelle <mitchellenjoki7@gmail.com>
2022-06-23 20:28:14 +0300

    Done.

--- main.js
+++ main.js
@@ -3,7 +3,19 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", function () {
+  const articles = document.getElementsByTagName("article");
 
+  const numberOfArticles = articles.length;
+
+  for (i = 0; i < numberOfArticles; i++) {
+    articles[i]
+      .getElementsByTagName("span")[1]
+      .addEventListener("click", function () {
+        console.log(articles[i]);
+      });
+  }
+});
 
 
 
