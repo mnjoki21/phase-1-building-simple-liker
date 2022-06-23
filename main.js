cecb05f9a9a3e57672fe277b5ac8885217db7578
mitchelle <mitchellenjoki7@gmail.com>
2022-06-23 21:41:22 +0300

    Done.

--- main.js
+++ main.js
@@ -15,7 +15,28 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log(articles[i]);
       });
   }
-});
+}); 
+then(function(){
+      if ( heart.innerText === EMPTY_HEART) {
+        heart.innerText = FULL_HEART;
+        heart.style.color = "red";
+        heart.className = "activated-heart";
+      } else {
+        heart.innerText = EMPTY_HEART;
+        heart.style.color = "";
+        heart.className = "";
+      }
+    })
+    .catch(function(error) { const modal = document.getElementById("modal");
+      modal.className = "";
+      modal.innerText = error;
+      setTimeout(() =>  modal.className = "hidden", 3000);
+    });
+
+for (const glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
+
 
 
 
